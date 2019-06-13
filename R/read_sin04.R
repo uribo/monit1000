c7bbacd47de3495bde6faeea8b08c98e87f789e3
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-06-13 14:12:47 +0900

    count to numeric

--- R/read_sin04.R
+++ R/read_sin04.R
@@ -4,17 +4,19 @@ read_sin04 <- function(path) {
                                                   locale = readr::locale(encoding = "cp932"),
                                                   n_max = 1))))
   if (col_n == 24L) {
-    readr::read_csv(path,
+    d <- readr::read_csv(path,
                     locale = readr::locale(encoding = "cp932"),
                     col_types = c("cccddccccdddddddddcccccc"))
-    mutate()
   } else if (col_n == 25L) {
-    readr::read_csv(path,
+    d <- readr::read_csv(path,
                     locale = readr::locale(encoding = "cp932"),
                     col_types = c("cccddcccccddddddddccccccc"))
   } else if (col_n == 28L) {
-    readr::read_csv(path,
+    d <- readr::read_csv(path,
                     locale = readr::locale(encoding = "cp932"),
                     col_types = c("cccddcccccddddddddcccccccccc"))
   }
+    d %>%
+      dplyr::mutate(`個体数` = dplyr::na_if(`個体数`, "\uff0d") %>%
+                      as.numeric())
 }
